765716478d9486653851d601b2747dab0e91b69c
Ellis Chang <ellischang@elliss-macbook-pro.local>
2019-02-12 19:14:07 -0800

    Initial Commit

---
+++ flixapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixapp
+//
+//  Created by Ellis Chang on 2/12/19.
+//  Copyright © 2019 echang41@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
